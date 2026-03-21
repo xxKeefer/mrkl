60a0a8c0176f6a090374e2b659236eded4a57dc2
Daniel John Keefer <xxkeefer@proton.me>
2026-03-21 23:32:40 +1000

    chore: add nix devshell with node 22 + pnpm (#45)

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "mrkl dev shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs_22
+          pkgs.pnpm
+        ];
+      };
+    };
+}
